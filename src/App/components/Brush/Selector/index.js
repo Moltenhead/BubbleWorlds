b247df962d454899e20710b2703e7aba5a0446bc
Charlie Gardai <charlie.gardai@plugandwork.fr>
2019-06-08 03:22:34 +0200

    feat : add Selector

---
+++ src/App/components/Brush/Selector/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import BaseComponent from "../../BaseComponent";
+
+class Selector extends BaseComponent {
+  render() {
+    return null;
+  }
+}
+
+export default Selector;
