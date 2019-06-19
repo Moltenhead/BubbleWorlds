e27d46e1094d74426be59bed7a90d3a9893c8fd0
Charlie Gardai <charlie.gardai@plugandwork.fr>
2019-06-19 21:02:25 +0200

    feat : added Bubble

--- src/App/index.js
+++ src/App/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./index.scss";
 
 import Canvas from "./components/Canvas";
+import Bubble from "./components/Bubble";
 
 function App() {
   return (
     <div className="App">
       <Canvas />
+      <Bubble />
     </div>
   );
 }
